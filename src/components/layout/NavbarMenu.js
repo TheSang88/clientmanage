800078b2ec91ce9023eccb3846496c9064d044a1
TheSang88 <128338833+TheSang88@users.noreply.github.com>
2023-10-25 10:06:10 +0700

    Update NavbarMenu.js

--- src/components/layout/NavbarMenu.js
+++ src/components/layout/NavbarMenu.js
@@ -44,61 +44,6 @@ const NavbarMenu = () => {
 				</Navbar.Collapse>
 			</Container>
 		</Navbar>
-
-
-
-
-
-
-
-
-
-
-
-		// <Navbar expand='lg' bg='primary' variant='dark' className='shadow'>
-
-		// 	<Navbar.Brand className='font-weight-bolder text-white'>
-		// 		<i class="fa-solid fa-cubes fa-2xl me-2 ms-2"></i>
-		// 		MANAGE
-		// 	</Navbar.Brand>
-		// 	<Nav className='mr-auto'>
-		// 		<Nav.Link
-		// 			className='font-weight-bolder text-white'
-		// 			to='/dashboard'
-		// 			as={Link}
-		// 		>
-		// 			Dashboard
-		// 		</Nav.Link>
-		// 		<Nav.Link
-		// 			className='font-weight-bolder text-white'
-		// 			to='/about'
-		// 			as={Link}
-		// 		>
-		// 			About
-		// 		</Nav.Link>
-		// 	</Nav>
-
-		// 	<Nav className='ms-auto p-1 fs-5 w-25'>
-		// 		<NavDropdown title={username} className='ms-auto me-auto'>
-		// 			<NavDropdown.Item>
-		// 				<Button
-		// 					variant='secondary'
-		// 					className='font-weight-bolder text-white'
-		// 					onClick={logout}
-		// 				>
-		// 					Logout
-		// 					<i class="fa-solid fa-right-from-bracket fa-2xl ms-2"></i>
-		// 				</Button>
-		// 			</NavDropdown.Item>
-		// 			<NavDropdown.Item>
-		// 				Doior
-		// 			</NavDropdown.Item>
-		// 		</NavDropdown>
-		// 	</Nav>
-
-
-
-		// </Navbar>
 	)
 }
 
